9027b730ea9d3cb4c43bcb2b53c707f1e1ea5038
Noeh <noehledra@gmail.com>
2024-07-02 20:11:06 +0200

    Suprression fichier CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // state
+  // const [name, setName] = useState("")
+  // const [inputValue, setInputValue] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Bonjour No
-        </p>
-      </div>
-    </>
-  )
+  // comportements
+  // const handleChange = (e) => {
+  //   const userName = e.target.value
+  //   setInputValue(userName)
+  //   setName(userName)
+  // }
+  
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   alert(`Bonjour ${name}`)
+  //   setInputValue('')
+  // }
+
+  // // render
+  // return (
+  //   <>
+  //     <h1>Bienvenue chez nous !</h1>
+  //     <h2>Connectez-vous</h2>
+  //     <form action="#" onSubmit={handleSubmit}>
+  //       <input type="text" placeholder="Entrez votre prénom..." onChange={handleChange} value={inputValue} required />
+  //       <button>Accédez à votre espace</button>
+  //     </form>
+  //   </>
+  // )
 }
 
 export default App
