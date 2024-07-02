ad215297c1d63f8255ca7c97693463dc23b3f4a3
Noeh <noehledra@gmail.com>
2024-07-02 22:03:51 +0200

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-// import { useState } from "react"
 import "./App.css"
+import LoginPage from "./components/pages/LoginPage"
 
 function App() {
-  // state
-  // const [name, setName] = useState("")
-  // const [inputValue, setInputValue] = useState("")
-
-  // comportements
-  // const handleChange = (e) => {
-  //   const userName = e.target.value
-  //   setInputValue(userName)
-  //   setName(userName)
-  // }
-  
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   alert(`Bonjour ${name}`)
-  //   setInputValue('')
-  // }
-
-  // // render
-  // return (
-  //   <>
-  //     <h1>Bienvenue chez nous !</h1>
-  //     <h2>Connectez-vous</h2>
-  //     <form action="#" onSubmit={handleSubmit}>
-  //       <input type="text" placeholder="Entrez votre prénom..." onChange={handleChange} value={inputValue} required />
-  //       <button>Accédez à votre espace</button>
-  //     </form>
-  //   </>
-  // )
+  return <LoginPage />
 }
 
 export default App
