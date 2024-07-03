9f35534ff73108936e3e0658380979ec02897578
Noeh <noehledra@gmail.com>
2024-07-03 21:57:13 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
+
 function LoginPage() {
-    return ( <div>Login Page</div> );
+    // STATE
+    const [inputValue, setInputValue] = useState("")
+
+    // BEHAVIOR
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Bonjour ${inputValue}`)
+        setInputValue('')
+    }
+
+    // RENDER
+    return (
+        <>
+            <h1>Bienvenue chez nous !</h1>
+            <br />
+            <h2>Connectez-vous</h2>
+            <form action="#" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Entrez votre prénom..." onChange={handleChange} value={inputValue} required />
+                <button>Accédez à votre espace</button>
+            </form>
+        </>
+    )
 }
 
 export default LoginPage;
