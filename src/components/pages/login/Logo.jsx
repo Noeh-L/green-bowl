f0dd62ee983761174a55b62bedb42438fd7a7cc2
Noeh <noehledra@gmail.com>
2024-07-16 20:41:58 +0200

    💄F03: LOGIN > Add fonts and style component Logo

--- src/components/pages/login/Logo.jsx
+++ src/components/pages/login/Logo.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import logo from "../../../assets/logo-orange.png";
+import { theme } from "../../../theme/index.js";
+
 function Logo() {
-  return <h1>CRAZEE BURGER</h1>;
+  return (
+    <TitleStyled>
+      <div>CRAZEE</div>
+      <img src={logo} alt="Logo d'un burger" />
+      <div>BURGER</div>
+    </TitleStyled>
+  );
 }
+
+const TitleStyled = styled.h1`
+  font-family: "Amatic SC", sans-serif;
+  color: ${theme.colors.primary};
+  font-size: ${theme.fonts.P6};
+  display: flex;
+  align-items: center;
+  gap: ${theme.spacing.sm};
+
+  img {
+    height: 80px;
+  }
+`;
 
 export default Logo;
