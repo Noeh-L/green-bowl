b0f7470d89743e9220a5cd37d29eab02801635d6
Noeh <noehledra@gmail.com>
2024-07-16 19:37:49 +0200

    💄 F03: LOGIN > Add style to background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import LoginForm from "./LoginForm";
 import Logo from "./Logo";
+import background from "../../../assets/burger-background.jpg";
 
 function LoginPage() {
   // RENDER
   return (
     <Container>
+      <BackgroundContainer>
+        <img src={background} alt="Burger et frites" />
+        <Overlay></Overlay>
+      </BackgroundContainer>
       <Logo />
       <LoginForm />
     </Container>
@@ -14,11 +19,33 @@ function LoginPage() {
 
 const Container = styled.div`
   height: 100vh;
-  border: 1px solid blue;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
+`;
+
+const BackgroundContainer = styled.div`
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: relative;
+  }
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  inset: 0;
+  background-color: rgba(0 0 0 / 0.6);
 `;
 
 export default LoginPage;
