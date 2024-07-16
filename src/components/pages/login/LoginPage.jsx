2d8c2025ccee9eb8a70f7f3caacb448ac8ba8a8a
Noeh <noehledra@gmail.com>
2024-07-16 13:19:28 +0200

    💄 F03: LOGIN > Add structural layout

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,14 +1,24 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
 import Logo from "./Logo";
 
 function LoginPage() {
   // RENDER
   return (
-    <div>
+    <Container>
       <Logo />
       <LoginForm />
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default LoginPage;
