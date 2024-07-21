d6486ced5fba9c57371dcf372dd032f51e4c76a1
Noeh <noehledra@gmail.com>
2024-07-21 13:51:17 +0200

    💄 F04: ORDER > Add layout and background

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,5 +1,15 @@
-function Navbar() {
-  return <div>NAVBAR</div>;
+import { Link } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
+function Navbar({ username }) {
+  return (
+    <div>
+      <h1>Bonjour {username}</h1>
+      <Link to={"/"}>
+        <button>Déconnexion</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,24 +1,46 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import styled from "styled-components";
+import { theme } from "../../../theme";
 
 function OrderPage() {
   // STATE
   const { username } = useParams();
 
-  // COMPORTEMENT
-
   // RENDER
   return (
-    <div>
-      <Navbar />
-      <Main />
-      <h1>Bonjour {username}</h1>
-      <Link to={"/"}>
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="background"></div>
+      <main>
+        <Navbar username={username} />
+        <Main />
+      </main>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & .background {
+    background: ${theme.colors.primary};
+    position: absolute;
+    inset: 0;
+    z-index: -1;
+  }
+
+  main {
+    background: white;
+    width: 1400px;
+    height: 95vh;
+    border-radius: ${theme.borderRadius.extraRound};
+  }
+`;
 
 export default OrderPage;
