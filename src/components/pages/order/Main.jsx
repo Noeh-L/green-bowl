341186e8d36bc52babeec92efc144b88ad919b69
Noeh <noehledra@gmail.com>
2024-07-22 14:13:19 +0200

    ♻️ F04: ORDER > Main > Add background color to Main

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { theme } from "../../../theme";
 
 function Main() {
   return <MainSyled></MainSyled>;
 }
 
 const MainSyled = styled.div`
-  background: #f5f5f7;
+  background: ${theme.colors.background_white};
   flex: 1;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 `;
