68bd4b270b3062cde66ea0022771ee5085f87e86
Noeh <noehledra@gmail.com>
2024-07-23 23:21:09 +0200

    🚧 F05: ORDER > Add layout with grid

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,13 +2,50 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 function Main() {
-  return <MainSyled></MainSyled>;
+  return (
+    <MainSyled>
+      {/* <div className="basket">Panier</div> */}
+      <div className="deck">
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+      </div>
+    </MainSyled>
+  );
 }
 
 const MainSyled = styled.div`
   background: ${theme.colors.background_white};
   flex: 1;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  display: flex;
+
+  .deck {
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: ${theme.spacing.xl} ${theme.spacing.xxl};
+    padding: ${theme.spacing.xl} ${theme.spacing.xxl};
+    grid-auto-rows: 330px;
+
+    .card {
+      border: 1px solid blue;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 `;
 
 export default Main;
