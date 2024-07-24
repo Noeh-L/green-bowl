420e51b90cd2fb040d985fdd364696b1619e8ec0
Noeh <noehledra@gmail.com>
2024-07-24 13:00:20 +0200

    ✨ F05: ORDER > Main > Create Card component

---
+++ src/components/pages/order/Card.jsx
@@ -0,0 +1,82 @@
+import PrimaryButton from "../../reusable-ui/PrimaryButton";
+import burger from "../../../assets/burger3.png";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+
+function Card() {
+  return (
+    <CardStyled>
+      <div className="picture">
+        <img src={burger} alt="Burger 1" />
+      </div>
+
+      <div className="infos">
+        <h2 className="infos-title">Burger And Soda Bacon Egg Fries</h2>
+        <div className="infos-add">
+          <p className="price">5,50€</p>
+          <PrimaryButton label={"Ajouter"} className="addButton" />
+        </div>
+      </div>
+    </CardStyled>
+  );
+}
+
+const CardStyled = styled.div`
+  background: ${theme.colors.white};
+  height: 330px;
+  width: 240px;
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.sm};
+  padding: ${theme.spacing.xl} ${theme.spacing.md} 0;
+  border-radius: ${theme.borderRadius.extraRound};
+  box-shadow: ${theme.shadows.card};
+
+  .picture {
+    width: 200px;
+    height: 145px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & img {
+      object-fit: contain;
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .infos {
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing.xxs};
+
+    &-title {
+      font-family: "Amatic SC", serif;
+      font-size: ${theme.fonts.P4};
+      font-weight: ${theme.weights.bold};
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    &-add {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .price {
+        color: ${theme.colors.primary};
+        font-size: ${theme.fonts.P0};
+        font-weight: ${theme.weights.regular};
+      }
+      .addButton {
+        font-size: ${theme.fonts.XS};
+        padding: ${theme.spacing.sm} ${theme.spacing.lg};
+        width: 95px;
+      }
+    }
+  }
+`;
+
+export default Card;
--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,28 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
-import PrimaryButton from "../../reusable-ui/PrimaryButton";
-import burger from "../../../assets/burger-bacon-egg.png";
+import Card from "./Card";
 
 function Main() {
   return (
     <MainSyled>
       {/* <div className="basket">Panier</div> */}
       <div className="deck">
-        <div className="card">
-          <div className="card__img">
-            <img src={burger} alt="Burger 1" />
-          </div>
-
-          <div className="card__infos">
-            <h2 className="card__infos-title">
-              Burger And Soda Bacon Egg Fries
-            </h2>
-            <div className="card__infos-add">
-              <p className="price">5,50€</p>
-              <PrimaryButton label={"Ajouter"} className="addButton" />
-            </div>
-          </div>
-        </div>
+        <Card />
       </div>
     </MainSyled>
   );
@@ -40,62 +25,6 @@ const MainSyled = styled.div`
     grid-template-columns: repeat(4, 1fr);
     gap: ${theme.spacing.xl} ${theme.spacing.xxl};
     padding: ${theme.spacing.xl} ${theme.spacing.xxl};
-
-    .card {
-      background: ${theme.colors.white};
-      height: 330px;
-      width: 240px;
-      display: flex;
-      flex-direction: column;
-      gap: ${theme.spacing.sm};
-      padding: ${theme.spacing.xl} ${theme.spacing.md} 0;
-      border-radius: ${theme.borderRadius.extraRound};
-      box-shadow: ${theme.shadows.card};
-
-      &__img {
-        width: 200px;
-        height: 145px;
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        & img {
-          object-fit: cover;
-          width: 100%;
-        }
-      }
-
-      &__infos {
-        display: flex;
-        flex-direction: column;
-        gap: ${theme.spacing.xxs};
-
-        &-title {
-          font-family: "Amatic SC", serif;
-          font-size: ${theme.fonts.P4};
-          font-weight: ${theme.weights.bold};
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-        &-add {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-
-          .price {
-            color: ${theme.colors.primary};
-            font-size: ${theme.fonts.P0};
-            font-weight: ${theme.weights.regular};
-          }
-          .addButton {
-            font-size: ${theme.fonts.XS};
-            padding: ${theme.spacing.sm} ${theme.spacing.lg};
-          }
-        }
-      }
-    }
   }
 `;
 
