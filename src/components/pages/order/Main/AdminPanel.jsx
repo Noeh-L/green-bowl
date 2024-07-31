bea99b6d9d066f0e7c790d2487a4ac4f9d900da4
Noeh <noehledra@gmail.com>
2024-08-01 00:08:32 +0200

    ✨ F07: ORDER > Main > AdminPane > Add condiionnal rendering to display the panel when admin mode is activated

--- src/components/pages/order/Main/AdminPanel.jsx
+++ src/components/pages/order/Main/AdminPanel.jsx
@@ -1,19 +1,23 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import { FiChevronDown } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
 import AdminTab from "../../../reusable-ui/AdminTab";
+import { useIsAdminModeContext } from "../../../../context/IsAdminContext";
 
 function AdminPanel() {
+  const { isAdminMode, setIsAdminMode } = useIsAdminModeContext();
+
   return (
-    <AdminPanelStyled>
-      <div className="tabs-container">
+    <AdminPanelStyled $isVisible={isAdminMode}>
+      <AdminTabs>
         <AdminTab Icon={FiChevronDown} />
         <AdminTab Icon={AiOutlinePlus} label={"Ajouter un produit"} />
         <AdminTab Icon={MdModeEditOutline} label={"Modifier un produit"} />
-      </div>
-      <div className="content"></div>
+      </AdminTabs>
+      <AdminContent></AdminContent>
     </AdminPanelStyled>
   );
 }
@@ -23,25 +27,24 @@ const AdminPanelStyled = styled.div`
   bottom: 0;
   width: 100%;
   height: 295px;
-  display: flex;
+  display: ${({ $isVisible }) => ($isVisible ? "flex" : "none")};
   flex-direction: column;
   filter: drop-shadow(0 0 10px rgba(0 0 0 / 0.4));
+`;
 
-  .tabs-container {
-    /* border: 3px solid blue; */
-    width: fit-content;
-    display: flex;
-    align-items: end;
-    gap: 1px;
-    margin-left: ${theme.spacing.xxl};
-  }
-
-  .content {
-    flex: 1;
-    border: 1px solid ${theme.colors.greyLight};
-    border-radius: 0 0 15px 15px;
-    background: ${theme.colors.white};
-  }
+const AdminTabs = styled.div`
+  /* border: 3px solid blue; */
+  width: fit-content;
+  display: flex;
+  align-items: end;
+  gap: 1px;
+  margin-left: ${theme.spacing.xxl};
+`;
+const AdminContent = styled.div`
+  flex: 1;
+  border: 1px solid ${theme.colors.greyLight};
+  border-radius: 0 0 15px 15px;
+  background: ${theme.colors.white};
 `;
 
 export default AdminPanel;
