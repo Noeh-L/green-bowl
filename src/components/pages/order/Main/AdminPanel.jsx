69631b7d67ac9bf48ac560227f38431d5c451f75
Noeh <noehledra@gmail.com>
2024-07-31 10:56:53 +0200

    🚧 F07: ORDER > Main > AdminPanel > Add layout

--- src/components/pages/order/Main/AdminPanel.jsx
+++ src/components/pages/order/Main/AdminPanel.jsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 function AdminPanel() {
-  return <AdminPanelStyled>ADMIN PANEL</AdminPanelStyled>;
+  return (
+    <AdminPanelStyled>
+      <div className="tabs"> </div>
+      <div className="content"> </div>
+    </AdminPanelStyled>
+  );
 }
 
 const AdminPanelStyled = styled.div`
@@ -10,6 +15,19 @@ const AdminPanelStyled = styled.div`
   bottom: 0;
   width: 100%;
   height: 295px;
+  display: flex;
+  flex-direction: column;
+
+  .tabs {
+    border: 3px solid blue;
+    height: 45px;
+  }
+
+  .content {
+    border: 3px solid red;
+    border-radius: 0 0 15px 15px;
+    flex: 1;
+  }
 `;
 
 export default AdminPanel;
