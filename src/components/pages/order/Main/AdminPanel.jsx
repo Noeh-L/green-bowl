9a992088e181a1c34398556a4495499444ebea5f
Noeh <noehledra@gmail.com>
2024-07-31 11:50:20 +0200

    🚧 F07: ORDER > Main > AdminPanel > Styling tabs

--- src/components/pages/order/Main/AdminPanel.jsx
+++ src/components/pages/order/Main/AdminPanel.jsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 function AdminPanel() {
   return (
     <AdminPanelStyled>
-      <div className="tabs"> </div>
+      <div className="tabs-container">
+        <div className="arrow tab">⬇</div>
+        <div className="addProduct tab">Ajouter un produit</div>
+        <div className="modifyProduct tab">Modifier un produit</div>
+      </div>
       <div className="content"> </div>
     </AdminPanelStyled>
   );
 }
 
 const AdminPanelStyled = styled.div`
-  background: lightblue;
   position: absolute;
   bottom: 0;
   width: 100%;
@@ -18,15 +22,37 @@ const AdminPanelStyled = styled.div`
   display: flex;
   flex-direction: column;
 
-  .tabs {
-    border: 3px solid blue;
-    height: 45px;
+  .tabs-container {
+    /* border: 3px solid blue; */
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    gap: 1px;
+    margin-left: ${theme.spacing.xxl};
+
+    .tab {
+      background: ${theme.colors.white};
+      min-width: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0 0;
+      border: 1px solid ${theme.colors.greyLight};
+      padding: 10px 20px;
+      cursor: pointer;
+
+      &.addProduct {
+        background: black;
+        color: white;
+      }
+    }
   }
 
   .content {
+    flex: 1;
     border: 3px solid red;
     border-radius: 0 0 15px 15px;
-    flex: 1;
+    background: ${theme.colors.white};
   }
 `;
 
