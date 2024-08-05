947aa757219c203b0ade043a55db8b1dbf4665a9
Noeh <noehledra@gmail.com>
2024-08-06 01:33:30 +0200

    🚧 F08: AddProduct > Add layout

--- src/components/pages/order/Main/Admin/AddProduct.jsx
+++ src/components/pages/order/Main/Admin/AddProduct.jsx
@@ -1,5 +1,55 @@
+import styled from "styled-components";
+import TextInput from "../../../../reusable-ui/TextInput.jsx";
+import { FaBurger, FaCamera, FaEuroSign } from "react-icons/fa6";
+import { useState } from "react";
+
 function AddProduct() {
-  return <div>Ajouter un produit</div>;
+  const [productName, setProductName] = useState("");
+
+  return (
+    <AddProductStyled>
+      <div className="productPreview">
+        <img src="#" alt="#" />
+      </div>
+      <form action="post">
+        <TextInput
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          Icon={FaBurger}
+          placeholder={"Entrez le nom du produit (ex.: Supra Burgerz)"}
+        />
+        <TextInput
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          Icon={FaCamera}
+          placeholder={"Entrez l'URL"}
+        />
+        <TextInput
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          Icon={FaEuroSign}
+          placeholder={"Entrez le prix"}
+        />
+      </form>
+    </AddProductStyled>
+  );
 }
+
+const AddProductStyled = styled.div`
+  border: 1px solid blue;
+  height: 100%;
+  display: flex;
+  gap: 1rem;
+
+  .productPreview {
+    border: 3px solid blue;
+    height: 120px;
+    width: 215px;
+  }
+
+  form {
+    border: 3px solid red;
+  }
+`;
 
 export default AddProduct;
