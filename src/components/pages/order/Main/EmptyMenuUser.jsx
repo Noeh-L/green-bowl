c4c487c8140c4f52c3ae59b9b3a26bf7d8b2e411
Noeh <noehledra@gmail.com>
2024-08-09 21:00:51 +0200

    💄F08: Menu > Add style to <EmptyMenuUser/>

--- src/components/pages/order/Main/EmptyMenuUser.jsx
+++ src/components/pages/order/Main/EmptyMenuUser.jsx
@@ -1,13 +1,30 @@
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+
 function EmptyMenuUser() {
   return (
-    <div>
+    <EmptyMenuUserStyled>
       <h2>Victime de notre succès ! :D</h2>
       <p>
         De nouvelles recettes sont en cours de préparation.
         <br />À très vite !
       </p>
-    </div>
+    </EmptyMenuUserStyled>
   );
 }
+
+const EmptyMenuUserStyled = styled.div`
+  font-family: "Amatic SC";
+  color: ${theme.colors.greyBlue};
+  font-size: ${theme.fonts.P4};
+
+  position: absolute;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 export default EmptyMenuUser;
