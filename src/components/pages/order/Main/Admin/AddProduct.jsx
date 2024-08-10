aff6c8c6e6b78d80d3aa3615985f60120d2772b1
Noeh <noehledra@gmail.com>
2024-08-10 19:25:20 +0200

    💄 F08: AddProduct > Add the message next to the addProduct button

--- src/components/pages/order/Main/Admin/AddProduct.jsx
+++ src/components/pages/order/Main/Admin/AddProduct.jsx
@@ -7,6 +7,7 @@ import { FaHamburger } from "react-icons/fa";
 import { MdOutlineEuro } from "react-icons/md";
 import { BsFillCameraFill } from "react-icons/bs";
 import { useOrderContext } from "../../../../../context/OrderPageContext.jsx";
+import { FiCheck } from "react-icons/fi";
 
 function AddProduct() {
   // STATE
@@ -78,10 +79,16 @@ function AddProduct() {
         placeholder={"Prix"}
         className={"text-inputs"}
       />
-      <PrimaryButton
-        label={"Ajouter un nouveau produit au menu"}
-        className={"add-button"}
-      />
+      <div className="addButton-and-message">
+        <PrimaryButton
+          label={"Ajouter un nouveau produit au menu"}
+          className={"addButton"}
+        />
+        <div className="message">
+          <FiCheck className="icon" />
+          <span>Ajouté avec succès !</span>
+        </div>
+      </div>
     </AddProductStyled>
   );
 }
@@ -129,17 +136,42 @@ const AddProductStyled = styled.form`
     }
   }
 
-  .add-button {
-    grid-column: 2 / 4;
-    width: fit-content;
-    padding: ${theme.spacing.xs} ${theme.spacing.lg};
-    background: #60bd4f;
-    border: 1px solid #60bd4f;
-    font-size: 14px;
+  .addButton-and-message {
+    grid-column: 2 / 5;
+    display: flex;
+    align-items: center;
+    gap: ${theme.spacing.md};
 
-    &:hover {
-      background: white;
-      color: #60bd4f;
+    .addButton {
+      width: fit-content;
+      padding: ${theme.spacing.xs} ${theme.spacing.lg};
+      background: ${theme.colors.success};
+      border: 1px solid ${theme.colors.success};
+      font-size: 14px;
+      height: 100%;
+      font-size: ${theme.fonts.XS};
+
+      &:hover {
+        background: white;
+        color: #60bd4f;
+      }
+    }
+
+    .message {
+      color: ${theme.colors.success};
+      display: flex;
+      align-items: center;
+      gap: ${theme.spacing.xxs};
+
+      .icon {
+        border: 1px solid ${theme.colors.success};
+        width: 18px;
+        height: 18px;
+        border-radius: 50%;
+      }
+      span {
+        font-size: ${theme.fonts.P0};
+      }
     }
   }
 `;
