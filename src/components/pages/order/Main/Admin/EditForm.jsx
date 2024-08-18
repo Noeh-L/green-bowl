2eac9adbb9bdac5e3a8e69697d085bccea4b9af1
Noeh <noehledra@gmail.com>
2024-08-18 21:56:05 +0200

    ✨ F09: EditForm > Add conditionnal rendering of the hint message

--- src/components/pages/order/Main/Admin/EditForm.jsx
+++ src/components/pages/order/Main/Admin/EditForm.jsx
@@ -1,26 +1,42 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import { HiCursorClick } from "react-icons/hi";
+import { useOrderContext } from "../../../../../context/OrderPageContext";
 
 function ModifyProduct() {
+  // state
+  const { menu, idCardSelected } = useOrderContext();
+
+  // behavior
+  const productToEdit = menu.find((item) => item.id === idCardSelected);
+
+  // render
   return (
     <ModifyProductStyled>
-      <span>Cliquer sur un produit pour le modifier</span>
-      <HiCursorClick />
+      {productToEdit ? (
+        console.log("productToEdit: ", productToEdit)
+      ) : (
+        <div className="hintMessage">
+          <span>Cliquer sur un produit pour le modifier</span>
+          <HiCursorClick />
+        </div>
+      )}
     </ModifyProductStyled>
   );
 }
 
 const ModifyProductStyled = styled.div`
-  color: ${theme.colors.greyBlue};
-  font-family: "Amatic SC", cursive;
-  font-size: ${theme.fonts.P3};
+  .hintMessage {
+    color: ${theme.colors.greyBlue};
+    font-family: "Amatic SC", cursive;
+    font-size: ${theme.fonts.P3};
 
-  display: flex;
-  align-items: center;
-  gap: ${theme.spacing.xs};
+    display: flex;
+    align-items: center;
+    gap: ${theme.spacing.xs};
 
-  margin-top: ${theme.spacing.xl};
+    margin-top: ${theme.spacing.xl};
+  }
 `;
 
 export default ModifyProduct;
