c498634c1d5fc5570967176e72b3f9a8943d5a3c
Noeh <noehledra@gmail.com>
2024-08-29 21:31:00 +0200

    ✨  F11: BASKET > On live update of product info when modification in edit form

--- src/components/pages/order/Main/Basket/BasketBody.jsx
+++ src/components/pages/order/Main/Basket/BasketBody.jsx
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import BasketCard from "./BasketCard";
+import { useOrderContext } from "../../../../../context/OrderPageContext";
 
 function BasketBody({ basket }) {
+  const { menu } = useOrderContext();
+
   return (
     <BasketBodyStyled>
-      {basket.map((product) => (
-        <BasketCard key={product.id} {...product} />
-      ))}
+      {basket.map((product) => {
+        // extract from menu the product that match the mapped one in basket
+        const productFromMenu = menu.find(
+          (productInMenu) => productInMenu.id === product.id,
+        );
+
+        return (
+          <BasketCard
+            key={product.id}
+            imageSource={productFromMenu.imageSource}
+            title={productFromMenu.title}
+            price={productFromMenu.price}
+            quantity={product.quantity}
+          />
+        );
+      })}
     </BasketBodyStyled>
   );
 }
