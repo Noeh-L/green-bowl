8a99291cf17d344d079ff92116795f3e0118bae3
Noeh <noehledra@gmail.com>
2024-08-29 18:53:06 +0200

    🚧 F11 : BASKET > Display all the item added in basket

--- src/components/pages/order/Main/Basket/BasketBody.jsx
+++ src/components/pages/order/Main/Basket/BasketBody.jsx
@@ -3,20 +3,32 @@ import styled from "styled-components";
 function BasketBody({ basket }) {
   return (
     <BasketBodyStyled>
-      <div>
-        <img className="imageSource" src={basket[0].imageSource} />
-        <div className="title">{basket[0].title}</div>
-        <div className="price">{basket[0].price}</div>
-        <div className="quantity">{basket[0].quantity}</div>
-      </div>
+      {basket.map((product) => (
+        <div className="basket-card" key={product.id}>
+          <img className="imageSource" src={product.imageSource} />
+          <div className="title">{product.title}</div>
+          <div className="price">{product.price}</div>
+          <div className="quantity">{product.quantity}</div>
+        </div>
+      ))}
     </BasketBodyStyled>
   );
 }
 
 const BasketBodyStyled = styled.div`
   flex: 1;
-  img {
-    width: 50px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .basket-card {
+    border: 4px solid blue;
+    width: 80%;
+
+    img {
+      width: 50px;
+    }
   }
 `;
 
