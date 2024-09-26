f85349a2839b6f09ed56d43fc4f868992032ca40
Noeh <noehledra@gmail.com>
2024-09-26 12:51:41 +0200

    ♻️  F13: Modify images path to work when deployed with vercel

--- src/enums/product.js
+++ src/enums/product.js
@@ -5,5 +5,4 @@ export const EMPTY_PRODUCT = Object.freeze({
   price: 0,
 });
 
-export const IMAGE_BY_DEFAULT =
-  "../../../../../../public/assets/coming-soon.png";
+export const IMAGE_BY_DEFAULT = "/assets/coming-soon.png";
