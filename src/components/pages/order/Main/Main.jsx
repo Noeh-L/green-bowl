1cabf40926760461d8e04abf95ca321ed4586695
Noeh <noehledra@gmail.com>
2024-10-09 10:36:27 +0200

    ✨ F14: ADMIN > Add animation when panel mount and unmount

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -5,6 +5,7 @@ import Admin from "./Admin/Admin";
 import { useOrderContext } from "../../../../context/OrderPageContext";
 import Menu from "./Menu/Menu";
 import Basket from "./Basket/Basket.jsx";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function Main() {
   const { isAdminMode } = useOrderContext();
@@ -14,7 +15,13 @@ function Main() {
       <Basket />
       <div className="menu-and-admin">
         <Menu />
-        {isAdminMode && <Admin />}
+        <TransitionGroup component={null}>
+          {isAdminMode && (
+            <CSSTransition classNames={"admin"} timeout={500}>
+              <Admin />
+            </CSSTransition>
+          )}
+        </TransitionGroup>
       </div>
     </MainSyled>
   );
@@ -32,6 +39,24 @@ const MainSyled = styled.div`
     overflow: hidden;
     position: relative;
     flex: 1;
+  }
+
+  .admin-enter {
+    bottom: -100%;
+  }
+  .admin-enter-active {
+    bottom: 0;
+    transition: 0.5s;
+  }
+  .admin-enter-done {
+  }
+
+  .admin-exit {
+    bottom: 0;
+  }
+  .admin-exit-active {
+    bottom: -100%;
+    transition: 0.5s;
   }
 `;
 
