c4199c01d799b707ca9014b45241889756e4c892
Noeh <noehledra@gmail.com>
2024-12-16 17:51:03 +0100

    ♻️ TSE-04: Add fakeMenu.ts

---
+++ src/types/Product.ts
@@ -0,0 +1,9 @@
+export type MenuProduct = {
+  id: string | number;
+  imageSource: string;
+  title: string;
+  price: number;
+  quantity: number;
+  isAvailable: boolean;
+  isAdvertised: boolean;
+};
