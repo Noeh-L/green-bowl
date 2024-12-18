b328f9e8a4e94d6e902d37fd537d9a3be12b1be3
Noeh <noehledra@gmail.com>
2024-12-18 19:49:28 +0100

    ♻️ TSE-05: Add product.ts

--- src/enums/product.ts
+++ src/enums/product.ts
@@ -1,13 +1,15 @@
-export const EMPTY_PRODUCT = Object.freeze({
+import { MenuProduct } from "@/types/Product";
+
+export const EMPTY_PRODUCT: MenuProduct = {
   id: "",
   title: "",
   imageSource: "",
   price: 0,
   isAvailable: true,
   isAdvertised: false,
-});
+};
 
 export const IMAGE_BY_DEFAULT = "/assets/coming-soon.png";
 export const IMAGE_NO_STOCK = "/assets/stock-epuise.png";
 
-export const UNAVAILABLE_MSG = "Non disponible";
+export const UNAVAILABLE_MSG = "Non disponible" as const;
