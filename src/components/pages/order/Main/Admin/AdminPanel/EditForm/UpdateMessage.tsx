b6bd15153a0f7a976c4197fa761c2e7da3acfdd6
Noeh <noehledra@gmail.com>
2024-12-23 01:04:46 +0100

    ♻️ TSE-14: Add InfoEditMessage.tsx and UpdateMessage.tsx

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm/UpdateMessage.tsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm/UpdateMessage.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "../../../../../../../theme";
+import { theme } from "@/theme";
 import { BsCloudCheck } from "react-icons/bs";
 
 function UpdateMessage() {
