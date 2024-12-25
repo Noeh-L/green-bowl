08315561e727173ca97e5bf799625c44110e56ed
Noeh <noehledra@gmail.com>
2024-12-25 17:25:09 +0100

    💄 N01: EmptyBaskert > Center text

--- src/components/pages/order/Main/Basket/BasketBody/EmptyBasket.tsx
+++ src/components/pages/order/Main/Basket/BasketBody/EmptyBasket.tsx
@@ -6,10 +6,10 @@ function EmptyBasket() {
 }
 
 const EmptyBasketStyled = styled.span`
-  font-size: 36px;
-  text-transform: capitalize;
+  font-size: ${theme.fonts.P4};
   color: ${theme.colors.greyBlue};
   flex: 1;
+  text-align: center;
 
   display: flex;
   align-items: center;
